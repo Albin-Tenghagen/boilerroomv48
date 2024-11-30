1a8c40de187d4600ba9eabbd2dedfa4df0280cfe
ZanaPK0 <timezana10@gmail.com>
2024-11-30 19:52:13 +0100

    changed date for api search to 2024-11-15 for more margin

--- js/index.js
+++ js/index.js
@@ -103,7 +103,7 @@ const fetchApiResults = async (type = "all") => {
       default:
         url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(
           type
-        )}&language=en&from=2024-11-5&sortBy=publishedAt&apiKey=1006e9f332db40bd8553b27720785488`;
+        )}&language=en&from=2024-11-15&sortBy=publishedAt&apiKey=1006e9f332db40bd8553b27720785488`;
         break;
     }
 
